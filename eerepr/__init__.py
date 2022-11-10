bb3105d009551764f3851285aa69e1929f1859ab
Aaron Zuspan <aa.zuspan@gmail.com>
2022-11-09 21:40:19 -0800

    Bumpversion 0.0.2

--- eerepr/__init__.py
+++ eerepr/__init__.py
@@ -3,7 +3,7 @@ import ee
 from eerepr.repr import initialize
 from eerepr.config import options
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __all__ = ['clear_cache', 'initialize', 'options']
 
 initialize(max_cache_size=options.max_cache_size)
