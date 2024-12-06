f52468b85af39c58ae78a595d3a8a80885a5f035
Aaron Zuspan <aa.zuspan@gmail.com>
2024-12-05 21:22:30 -0800

    Swap JS for summary-details tags

--- eerepr/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleHeader() {
-    const parent = this.parentElement;
-    parent.className = parent.className === "ee-open" ? "ee-shut" : "ee-open";
-}
-
-for (let c of document.getElementsByClassName("ee-toggle")) {
-    c.onclick = toggleHeader;
-}
