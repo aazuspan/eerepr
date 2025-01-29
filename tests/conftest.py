eecc5d9d7858d59534e54d43b1c00dda5e55f68f
Aaron Zuspan <50475791+aazuspan@users.noreply.github.com>
2025-01-29 08:32:12 -0800

    Use service account for CI (#56)
    * Use service account for CI tests
    
    * Include service account secret
    
    * Parse project_id from service account key data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import json
+import os
 import warnings
 from collections import UserDict
 from pathlib import Path
@@ -65,7 +66,22 @@ def object_cache():
 
 
 def pytest_sessionstart(session):
-    ee.Initialize()
+    _init_ee_for_tests()
+
+
+def _init_ee_for_tests():
+    # Use the Github Service Account for CI tests
+    if os.environ.get("GITHUB_ACTIONS"):
+        key_data = os.environ.get("EE_SERVICE_ACCOUNT")
+        project_id = json.loads(key_data).get("project_id")
+        credentials = ee.ServiceAccountCredentials(None, key_data=key_data)
+    # Use stored persistent credentials for local tests
+    else:
+        # Project should be parsed from credentials
+        project_id = None
+        credentials = "persistent"
+
+    ee.Initialize(credentials, project=project_id)
 
 
 @pytest.fixture(autouse=True)
