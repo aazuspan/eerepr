dea4f4a462582c2d91ee48270283c1160dce3a63
Aaron Zuspan <aa.zuspan@gmail.com>
2025-01-29 08:16:51 -0800

    Parse project_id from service account key data

--- tests/conftest.py
+++ tests/conftest.py
@@ -66,13 +66,22 @@ def object_cache():
 
 
 def pytest_sessionstart(session):
+    _init_ee_for_tests()
+
+
+def _init_ee_for_tests():
+    # Use the Github Service Account for CI tests
     if os.environ.get("GITHUB_ACTIONS"):
-        service_account = os.environ.get("EE_SERVICE_ACCOUNT")
-        credentials = ee.ServiceAccountCredentials(None, key_data=service_account)
+        key_data = os.environ.get("EE_SERVICE_ACCOUNT")
+        project_id = json.loads(key_data).get("project_id")
+        credentials = ee.ServiceAccountCredentials(None, key_data=key_data)
+    # Use stored persistent credentials for local tests
     else:
+        # Project should be parsed from credentials
+        project_id = None
         credentials = "persistent"
 
-    ee.Initialize(credentials=credentials)
+    ee.Initialize(credentials, project=project_id)
 
 
 @pytest.fixture(autouse=True)
